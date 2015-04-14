3233b73b0a81300776fc02a21f00a57bc5254f12
santilla001 <santilla001@knights.gannon.edu>
2015-04-14 12:07:50 -0400

    Update calculatortest.php

--- calculatortest.php
+++ calculatortest.php
@@ -3,8 +3,8 @@ require_once('calculator.php');
 
 class CalcTest extends calc
 {
-    $number1 = 1;
-    $number2 = 2;
+    var $number1 = 1;
+    var $number2 = 2;
     
     public function add($number1, $number2)
     {
