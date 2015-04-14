2e5281f381a79aa9c0f79426717e6b70804240cc
santilla001 <santilla001@knights.gannon.edu>
2015-04-14 11:21:43 -0400

    Update calculatortest.php

--- calculatortest.php
+++ calculatortest.php
@@ -1,1 +1,19 @@
+<?php
+class CalcTest extends PHPUnit_Framework_TestCase
+{
+    public function calc($number1, $number2, $result)
+    {
+        $this->assertEquals($result, $number1 + $number2);
+    }
 
+    public function additionProvider()
+    {
+        return array(
+          array(0, 0, 0),
+          array(0, 1, 1),
+          array(1, 0, 1),
+          array(1, 1, 3)
+        );
+    }
+}
+?>
