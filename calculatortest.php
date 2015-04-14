7814d64ad7b005a069665a9a3d5c7e1b5c682405
santilla001 <santilla001@knights.gannon.edu>
2015-04-14 12:56:12 -0400

    Update calculatortest.php

--- calculatortest.php
+++ calculatortest.php
@@ -1,12 +1,12 @@
 <?php
-require_once('calculatortest.php')
+require_once('calculatortest.php');
 class CalcTest extends calc
 {
     public function TestAdd()
     {
       	$calc = new calc();
-		$result = $calc->add(2, 2);
-		$this->assertEquals(4, $result);
+	$result = $calc->add(2, 2);
+	$this->assertEquals(4, $result);
     }
 }
 ?>
