e68f9e7913826ca64122b2eb82456d2bcf7d313e
santilla001 <santilla001@knights.gannon.edu>
2015-04-14 11:49:26 -0400

    Update calculatortest.php

--- calculatortest.php
+++ calculatortest.php
@@ -1,20 +1,10 @@
 <?php
 require_once('calculatortest.php')
-//class CalcTest extends calc
-//{
-    public function add($number1, $number2)
+class CalcTest extends calc
+{
+    public function TestAdd($number1, $number2)
     {
         $this->assertEquals($number1 + $number2);
     }
-
-    public function additionProvider()
-    {
-        return array(
-          array(0, 0),
-          array(0, 1),
-          array(1, 0),
-          array(1, 1)
-        );
-    }
-//}
+}
 ?>
